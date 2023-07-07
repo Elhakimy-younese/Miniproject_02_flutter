c065377ea3ce1152d7fb5eb06af9b15bab5b2ee7
younes <elhakimyyouness4@gmail.com>
2023-07-07 20:17:11 +0100

    task3

--- mimiproject02flutter/lib/main.dart
+++ mimiproject02flutter/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: '',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -23,7 +22,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -36,14 +34,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final String fullname = Faker().generateFullName();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme
-              .of(context)
-              .colorScheme
-              .inversePrimary,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("mini project 02"),
         ),
         body: Padding(
@@ -61,31 +57,35 @@ class _MyHomePageState extends State<MyHomePage> {
                   Row(
                     children: [
                       const Text("Hi ", style: TextStyle(fontSize: 15)),
-                      Text( " $fullname " ,
-                        style: TextStyle(fontSize: 20,),),
+                      Text(
+                        " $fullname ",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          decoration: TextDecoration.underline,
+                        ),
+
+                      ),
                       const Spacer(),
                       GestureDetector(
-                        onTap: () {
-                          print("hello");
-                        },
-                        child: const Icon(
-                          Icons.info, color: Colors.blueAccent,)
-                      )
+                          onTap: () {
+                            print("hello");
+                          },
+                          child: const Icon(
+                            Icons.info,
+                            color: Colors.blueAccent,
+                          ))
                     ],
                   ),
                   const Spacer(),
-                  ElevatedButton(onPressed: () {
-                    print("hi   ");
-                  }, child: const Text("Go"))
+                  ElevatedButton(
+                      onPressed: () {
+                        print("hi   ");
+                      },
+                      child: const Text("Go"))
                 ],
               ),
             ),
           ),
-
-        )
-    );
+        ));
   }
-
 }
-
-
